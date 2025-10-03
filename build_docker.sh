533f8c2a1ce694a7d656f952ba3c92e3c2992c13
agirbau <andreu.girbau@gmail.com>
2025-10-03 13:58:48 +0900

    Running example

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Download relevant data
+# download only if missing
+[ -f pretrained/E2VID_lightweight.pth.tar ] || \
+    wget -c http://rpg.ifi.uzh.ch/data/E2VID/models/E2VID_lightweight.pth.tar -O pretrained/E2VID_lightweight.pth.tar
+
+[ -f data/dynamic_6dof.zip ] || \
+    wget -c http://rpg.ifi.uzh.ch/data/E2VID/datasets/ECD_IJRR17/dynamic_6dof.zip -O data/dynamic_6dof.zip
+
 # Build docker image
 docker build --no-cache -t e2vid:latest -f docker_env/Dockerfile .
