2b67607afb18e902605db4e8c53ffd35aebfe46b
agirbau <andreu.girbau@gmail.com>
2025-10-03 15:27:03 +0900

    Changed to docker-compose

--- docker_env/run_docker.sh
+++ docker_env/run_docker.sh
@@ -1,17 +1,3 @@
-# Not used anymore, see docker-compose.yaml
 #!/bin/bash
 
-E2VID_PATH=/home/andreu/work/projects/research/rpg_e2vid
-E2VID_DATA=/home/andreu/datasets
-
-# Add your Docker commands here
-docker rm e2vid
-docker run \
-    --name e2vid \
-    --gpus '"device=1"' \
-    -it \
-    -v $E2VID_PATH:/home/user/app \
-    -v $E2VID_DATA:/home/user/datasets \
-    -e E2VID_PATH=/home/user/app \
-    -e E2VID_DATA=/home/user/datasets \
-    e2vid
+docker-compose up
