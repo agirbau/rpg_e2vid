294bd65fc67119d7c0c73d2c3d21ad1944cc0a41
agirbau <andreu.girbau@gmail.com>
2025-10-07 12:42:19 +0900

    Change to dynamic port and reload server

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello from e2vid"}
+    return {"message": "Hello from e2vid 3.0 inference server!"}
 
 if __name__ == "__main__":
+    # TODO: adapt "run_reconstruction.py" logic to serve requests
     uvicorn.run(app, host="0.0.0.0", port=8000)
